605c9941589612a8d305b6ae25ca5dd22bc8106b
eddyjav <eddy.j.av@gmail.com>
2020-03-10 11:54:40 -0500

    Funciones y Parametros

--- PrimerProyecto_FP/src/primerproyecto_fp/PrimerProyecto_FP.java
+++ PrimerProyecto_FP/src/primerproyecto_fp/PrimerProyecto_FP.java
@@ -20,8 +20,18 @@ public class PrimerProyecto_FP {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        String cad = "cdm";
+        String cad = "Hello world ";
         System.out.println(cad);
+        imprimirNombre();        
+        //sumar2numeros(1,2);
+        System.out.println(sumar2numeros(1,2));
     }
-    
+     public static void imprimirNombre (){
+         System.out.println("Fundamentos");
+         }
+     
+     public static int sumar2numeros (int num1, int num2){
+         int suma = num1 + num2;
+         return suma;  
+         }
 }
